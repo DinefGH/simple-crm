11b429e33769ae367a03d88e4772eb105b29c312
DinefGH <fabianduerr@web.de>
2023-09-12 01:09:39 +0200

    user css

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -18,44 +18,86 @@ export class UserDetailComponent implements OnInit{
 
   constructor (public dialog: MatDialog, private route: ActivatedRoute, private firestore: AngularFirestore ) {}
 
-  ngOnInit() {
-    this.route.paramMap.subscribe( paramMap => {
-      const id = this.userId = paramMap.get('id')!;
-      console.log('GOT User ID', this.userId);
-      this.getUser();
-  })
-  }
+  /**
+ * Initializes the component.
+ * Subscribes to route parameters to retrieve the user ID.
+ */
+ngOnInit() {
+  this.route.paramMap.subscribe(paramMap => {
+    const id = this.userId = paramMap.get('id')!;
+    console.log('GOT User ID', this.userId);
+    this.getUser();
+  });
+}
 
-
-  getUser() {
-    this.firestore
+/**
+ * Fetches user details from Firestore based on the user ID.
+ */
+getUser() {
+  this.firestore
     .collection('users')
     .doc(this.userId)
     .valueChanges()
-    .subscribe(( userParam: any) => {
+    .subscribe((userParam: any) => {
       this.user = new User(userParam);
       console.log('Retrieved user', this.user);
     });
-  }
+}
 
-
-  deleteUserDetail() {
-    this.firestore
+/**
+ * Deletes the user details from Firestore based on the user ID.
+ */
+deleteUserDetail() {
+  this.firestore
     .collection('users')
     .doc(this.userId)
     .delete()
     .then(() => {
-        console.log('User successfully deleted!');
+      console.log('User successfully deleted!');
     })
     .catch((error) => {
-        console.error('Error removing user: ', error);
+      console.error('Error removing user: ', error);
     });
 }
- 
 
-  editUserDetail() {
-    const dialog = this.dialog.open(DialogEditUserComponent);
-    dialog.componentInstance.user = new User(this.user.toJSON());
-    dialog.componentInstance.userId = this.userId;
-  }
+/**
+ * Opens a dialog to edit user details.
+ */
+editUserDetail() {
+  const dialog = this.dialog.open(DialogEditUserComponent);
+  dialog.componentInstance.user = new User(this.user.toJSON());
+  dialog.componentInstance.userId = this.userId;
+}
+
+/**
+ * Generates a random RGB color that is not too bright.
+ * @returns {string} The generated RGB color string.
+ */
+getRandomColor(): string {
+  let color: string;
+  let r: number;
+  let g: number;
+  let b: number;
+
+  do {
+    r = Math.floor(Math.random() * 256);
+    g = Math.floor(Math.random() * 256);
+    b = Math.floor(Math.random() * 256);
+    color = `rgb(${r},${g},${b})`;
+  } while (this.isTooBright(r, g, b));
+
+  return color;
+}
+
+/**
+ * Checks if the provided RGB color is too bright.
+ * @param {number} r - The red component of the color.
+ * @param {number} g - The green component of the color.
+ * @param {number} b - The blue component of the color.
+ * @returns {boolean} True if the color is too bright, false otherwise.
+ */
+isTooBright(r: number, g: number, b: number): boolean {
+  const threshold = 100; 
+  return r > threshold || g > threshold || b > threshold;
+}
 }
